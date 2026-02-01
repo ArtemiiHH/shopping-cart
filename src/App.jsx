8a0b2f082a10bb4e73db2a0cedaa8a4db2efa0d1
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-01 17:33:08 +0200

    Add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+// Import styles
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+
+// Import components
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
