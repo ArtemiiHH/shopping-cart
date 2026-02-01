cd7b94424eb7be0d888a1e654fc45ab54a181525
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-01 16:54:39 +0200

    Create directory for each component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar";
+
+export default function Header() {
+  return (
+    <>
+      <NavBar></NavBar>
+      <h1>Logo</h1>
+      <button>Cart</button>
+    </>
+  );
+}
