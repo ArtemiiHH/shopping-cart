8bd0d9e8ddd46409d3731b0432cf3ba569f5c293
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-01 17:32:12 +0200

    Add styling to header module

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
-import NavBar from "../NavBar";
+import NavBar from "../navbar/NavBar";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <>
+    <header className={styles.myheader}>
       <NavBar></NavBar>
       <h1>Logo</h1>
       <button>Cart</button>
-    </>
+    </header>
   );
 }
