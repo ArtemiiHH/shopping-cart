024dd4ef49c1f64eb04bee08d29a200f597fc0ad
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-04 14:35:47 +0200

    Remove unused Outlet Import

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,5 +1,4 @@
 import styles from "./Content.module.css";
-import { Outlet } from "react-router";
 
 export default function Content({ children }) {
   return <section className={styles.mycontent}>{children}</section>;
