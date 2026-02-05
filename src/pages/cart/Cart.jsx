0bada271c82b95e672c5737f538b7b64a2a01633
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-05 20:32:27 +0200

    Get basic item sorting

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -10,6 +10,9 @@ export default function Cart() {
       <div className={styles.grid}>
         {/* Show empty text */}
         {cartItems.length === 0 ? <h3>Your cart is empty</h3> : <div></div>}
+        {cartItems.map((item) => (
+          <div key={item.id} className={styles.item}>{console.log(item)}</div>
+        ))}
       </div>
     </section>
   );
