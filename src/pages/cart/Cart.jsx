c1ae097ccbdf1731402c0118258d6ee2ab9b92b8
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-05 22:20:19 +0200

    Rewrite Cart using ternary op

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -7,14 +7,18 @@ export default function Cart() {
   return (
     <section>
       <h2>Selected items</h2>
+
       <div className={styles.grid}>
         {/* Show empty text */}
-        {cartItems.length === 0 ? <h3>Your cart is empty</h3> : <div></div>}
-        {cartItems.map((item) => (
-          <div key={item.id} className={styles.item}>
-            <img src={item.thumbnail} alt="" />
-          </div>
-        ))}
+        {cartItems.length === 0 ? (
+          <h3>Your cart is empty</h3>
+        ) : (
+          cartItems.map((item) => (
+            <div key={item.id} className={styles.item}>
+              <img src={item.thumbnail} alt={item.title} />
+            </div>
+          ))
+        )}
       </div>
     </section>
   );
