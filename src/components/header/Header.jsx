41195637fb82f7617b2a32599b2f44b81c63a54f
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-06 17:02:00 +0200

    Add comments

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,16 +3,25 @@ import styles from "./Header.module.css";
 import { Link } from "react-router";
 
 export default function Header({ cartItems }) {
+  // Count total items
+  const totalQty = cartItems.reduce((sum, item) => sum + item.qty, 0);
+
   return (
+    // Header
     <header className={styles.header}>
+      {/* Navigation Bar */}
       <NavBar></NavBar>
+
+      {/* Logo */}
       <h1 className={styles.logo}>
         <Link to="/">wwwatch.com</Link>
       </h1>
+
+      {/* Cart Link */}
       <ul>
         <li>
           <Link to="cart">
-            {cartItems.length === 0 ? "Cart" : `Cart (${cartItems.length})`}
+            {cartItems.length === 0 ? "Cart" : `Cart (${totalQty})`}
           </Link>
         </li>
       </ul>
