a7c349c01fcacce2f865e82791adc66b30558bb0
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-07 12:46:42 +0200

    Move grid div lower

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -8,40 +8,41 @@ export default function Cart() {
     <section>
       <h2>Selected items</h2>
 
-      <div className={styles.grid}>
-        {/* Show empty text */}
-        {cartItems.length === 0 ? (
-          <h3 className={styles.emptyText}>Your cart is empty</h3>
-        ) : (
-          // Each card
-          cartItems.map((item) => (
-            <>
-              <div className={styles.grid}>
-                <div key={item.cardItemId} className={styles.card}>
-                  {/* Item Image */}
-                  <img src={item.thumbnail} alt={item.title} />
-                  <div>
-                    {/* Item title */}
-                    <h3>{item.title}</h3>
-                    {/* Item price */}
-                    <h4>Total price: ${item.price * item.qty}</h4>
-                    {/* Item quantity */}
-                    <p>Quantity: {item.qty}</p>
-                  </div>
-                </div>
-                <div className={styles.totalBox}>
-                  {/* Order summary */}
-                  <h2>Order summary:</h2>
-                  {/* Total price */}
-                  <h3>{`Total:$${item.price * item.qty}`}</h3>
-                  {/* Subtotal */}
-                  <p>{`Subtotal x (${item.qty} items)`}</p>
+      {/* Show empty text */}
+      {cartItems.length === 0 ? (
+        <h3 className={styles.emptyText}>Your cart is empty</h3>
+      ) : (
+        <>
+          {/* Each item */}
+          {cartItems.map((item) => (
+            <div className={styles.grid}>
+              {/* Cart grid */}
+              <div key={item.cardItemId} className={styles.card}>
+                {/* Item Image */}
+                <img src={item.thumbnail} alt={item.title} />
+                <div>
+                  {/* Item title */}
+                  <h3>{item.title}</h3>
+                  {/* Item price */}
+                  <h4>Total price: ${item.price * item.qty}</h4>
+                  {/* Item quantity */}
+                  <p>Quantity: {item.qty}</p>
                 </div>
               </div>
-            </>
-          ))
-        )}
-      </div>
+
+              {/* Total amount box */}
+              <div className={styles.totalBox}>
+                {/* Order summary */}
+                <h2>Order summary:</h2>
+                {/* Total price */}
+                <h3>{`Total:$${item.price * item.qty}`}</h3>
+                {/* Subtotal */}
+                <p>{`Subtotal x (${item.qty} items)`}</p>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </section>
   );
 }
