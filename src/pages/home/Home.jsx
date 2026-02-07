11606ffafd24795f0221df7bfb145f332ed089f8
ArtemiiHH <artemiibarashkov2003@gmail.com>
2026-02-07 15:58:47 +0200

    Add box shadows to header and footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,14 +4,20 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <section className={styles.home}>
-      <div className="left-side">
+      {/* Home Page */}
+      {/* Left side */}
+      <div className={styles.leftColumn}>
+        {/* Title */}
         <h1 className="title">Best watch store</h1>
+        {/* Description */}
         <p className="description">Shop the best watches on the internet.</p>
+        {/* Shop Now Button */}
         <button className="shop-btn">
           <Link to="shop">Shop now</Link>
         </button>
       </div>
 
+      {/* Right side */}
       <div className="right-side">
         <img src="../src/assets/images/watch.jpg" />
       </div>
